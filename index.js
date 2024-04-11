7cc63236ffb2d967c2134e1d59d0c57efca29463
Maddie Adair <98988491+maddieadair@users.noreply.github.com>
2024-04-11 15:15:33 -0500

    test

--- index.js
+++ index.js
@@ -15,14 +15,19 @@ const tickets = require("./routes/tickets");
 const auth = require("./authentication/auth");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+  
+    // Handle preflight OPTIONS request
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
 
   if (req.method === "OPTIONS") {
     res.writeHead(200);
